6062ce750314c74f3ba7ab647b52667526f689a6
ann ngatia <annngatia94@gmail.com>
2025-08-12 12:47:50 +0300

    Create Anagram class with initializer to store original word

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,20 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word.lower()
+
+    def match(self, possible_anagrams):
+        matches = []
+
+        for candidate in possible_anagrams:
+            candidate_lower = candidate.lower()
+
+            if candidate_lower == self.word:
+                continue
+
+            if sorted(candidate_lower) == sorted(self.word):
+                matches.append(candidate)
+
+        return matches           
+
+        
